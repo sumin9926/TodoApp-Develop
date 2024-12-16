b9c1c99b17efd050ccd435c4c6ca0f478ec7b3e3
임PRO스터\su992 <su9926@naver.com>
2024-12-16 09:39:33 +0900

    init

---
+++ src/main/java/com/example/newtodoapp/NewTodoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.newtodoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewTodoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewTodoAppApplication.class, args);
+	}
+
+}
