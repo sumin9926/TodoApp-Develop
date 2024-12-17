1c65630e163c4ebcc2b7dba388721461ea9a7564
임PRO스터\su992 <su9926@naver.com>
2024-12-17 09:23:36 +0900

    :tada: Create new BaseEntity

---
+++ src/main/java/com/example/newtodoapp/entity/BaseEntity.java
@@ -0,0 +1,22 @@
+package com.example.newtodoapp.entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+	@CreatedDate
+	@Column(updatable = false)
+	private LocalDateTime createdDate;
+
+}
