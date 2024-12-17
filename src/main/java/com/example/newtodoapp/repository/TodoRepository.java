c973014f9d0989dfd6c3b48dc76fd49cee13f115
임PRO스터\su992 <su9926@naver.com>
2024-12-18 03:05:32 +0900

    :hammer: Remove unused imports

--- src/main/java/com/example/newtodoapp/repository/TodoRepository.java
+++ src/main/java/com/example/newtodoapp/repository/TodoRepository.java
@@ -2,7 +2,6 @@ package com.example.newtodoapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.example.newtodoapp.entity.Member;
 import com.example.newtodoapp.entity.Todo;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
