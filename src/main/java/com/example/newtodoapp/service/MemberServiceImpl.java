94d0ca226de7792c481c99f866ebf41d77973244
임PRO스터\su992 <su9926@naver.com>
2024-12-18 12:23:03 +0900

    :sparkles: Encoding password and Implement update member API

--- src/main/java/com/example/newtodoapp/service/MemberServiceImpl.java
+++ src/main/java/com/example/newtodoapp/service/MemberServiceImpl.java
@@ -1,15 +1,18 @@
 package com.example.newtodoapp.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import com.example.newtodoapp.dto.memberDto.MemberRequestDto;
 import com.example.newtodoapp.dto.memberDto.MemberResponseDto;
+import com.example.newtodoapp.encoder.PasswordEncoder;
 import com.example.newtodoapp.entity.Member;
 import com.example.newtodoapp.entity.Todo;
 import com.example.newtodoapp.repository.MemberRepository;
 
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 
 @Service
@@ -18,11 +21,14 @@ public class MemberServiceImpl implements MemberService {
 
 	private final MemberRepository memberRepository;
 	private final TodoService todoService;
+	private final PasswordEncoder passwordEncoder;
 
 	@Override
 	public MemberResponseDto signUp(MemberRequestDto dto) {
 
-		Member member = new Member(dto.getName(), dto.getEmail(), dto.getPassword());
+		String encodedPassword= passwordEncoder.encode(dto.getPassword());
+
+		Member member = new Member(dto.getName(), dto.getEmail(), encodedPassword);
 
 		Member savedMember = memberRepository.save(member);
 
@@ -59,18 +65,34 @@ public class MemberServiceImpl implements MemberService {
 	@Override
 	public void deleteMember(Long id) {
 
-		Member member=memberRepository.findMemberByIdOrElseThrow(id);
-		List<Todo> findTodo=todoService.findTodoByMemberId(id);
+		Member member = memberRepository.findMemberByIdOrElseThrow(id);
+		List<Todo> findTodo = todoService.findTodoByMemberId(id);
 
-		for(Todo todo:findTodo){ //회원 삭제 전 회원이 생성한 모든 일정 삭제
+		for (Todo todo : findTodo) { //회원 삭제 전 회원이 생성한 모든 일정 삭제
 			todoService.deleteTodo(todo.getId());
 		}
 
 		memberRepository.delete(member);
 	}
 
+	@Transactional
 	@Override
 	public MemberResponseDto updateMemberById(Long id, MemberRequestDto dto) {
-		return null;
+
+		Member member = memberRepository.findMemberByIdOrElseThrow(id);
+
+		Optional.ofNullable(dto.getName()).ifPresent(member::setName); //이름 수정
+		Optional.ofNullable(dto.getEmail()).ifPresent(member::setEmail); //이메일 수정
+		Optional.ofNullable(dto.getPassword()).ifPresent(password -> { //비밀번호 수정
+			String encodedPassword = passwordEncoder.encode(password);
+			member.setPassword(encodedPassword);
+		});
+
+		return new MemberResponseDto(
+			member.getId(),
+			member.getName(),
+			member.getEmail(),
+			member.getCreatedDate()
+		);
 	}
 }
